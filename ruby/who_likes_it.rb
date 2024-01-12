c827833f41703fef3e0237e2c01b9f58b373ddad
Johnny Carvalho <johnny.carvalho@contaswap.com>
2024-01-11 22:34:56 -0300

    Add solved ruby kata

---
+++ ruby/who_likes_it.rb
@@ -0,0 +1,15 @@
+def likes(names)
+  num_people = names.size
+  case num_people
+  when 0
+    'no one likes this'
+  when 1
+    "#{names[0]} likes this"
+  when 2
+    "#{names[0]} and #{names[1]} like this"
+  when 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{num_people - 2} others like this"
+  end
+end
